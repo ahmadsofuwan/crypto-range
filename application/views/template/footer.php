6d59db47153be4d407fbd03e7e45b4fd609c7787
ahmadsofuwan <69912778+ahmadsofuwan@users.noreply.github.com>
2022-10-21 01:19:08 +0700

    pwer by

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -8,7 +8,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; 2022 Power Ahmad Sofuwan</span>
+            <span>Copyright &copy; 2022 Power My Matic</span>
         </div>
     </div>
 </footer>
